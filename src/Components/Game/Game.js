607c1fb629326438baaba6bfb71b540e11cc00a4
Jane <jane.anqi.cui@gmail.com>
2022-04-10 12:20:19 -0400

    added comments to game component

--- src/Components/Game/Game.js
+++ src/Components/Game/Game.js
@@ -3,9 +3,7 @@ import './Game.css';
 import Board from '../Board/Board';
 import Keyboard from '../Keyboard/Keyboard';
 import { useState, useEffect } from 'react';
-import { WORDS} from '../../constants/wordlist';
-import { acceptedWords } from '../../constants/acceptedwords';
-import {getKeyword, isValidGuess, updateLetters} from '../helpers/helpers'
+import { getKeyword, isValidGuess, updateLetters } from '../helpers/helpers'
 
 const Game = () => {
 
@@ -13,27 +11,35 @@ const Game = () => {
   const [keyword, setKeyword] = useState('dodge');
   // track which guess they are at (out of 5)
   const [wordNumber, setWordNumber] = useState(0);
-  //letter number
+  // track which letter they are at 
   const [letterNumber, setLetterNumber] = useState(0);
-  const [wordStatuses, setWordStatuses] = useState(Array(6).fill(0))
+  // track the status of each word 
+  const [wordStatuses, setWordStatuses] = useState(Array(6).fill(0));
   let wordSize = 5;
   //as they type out letters for a current guess, build the guess string
   const [guesses, setGuesses] = useState(Array(6).fill(Array(5).fill('')));
+  
+  // track the status of each letter - at the start, none have been used
   const letterDict = {};
   const alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
   for (const key of alphabet) {
-    letterDict[key] = 3
+    // TODO: Change letters to descriptive strings
+    letterDict[key] = 3;
   }
 
-  const [letterStatus, setLetterStatus] = useState(letterDict)
+  const [letterStatus, setLetterStatus] = useState(letterDict);
 
+  // set keyword to guess when the game loads
   useEffect(() => {
     setKeyword(getKeyword());
   }, []);
 
+  // called when a letter on keyboard is clicked
   const buttonInteraction = (letter) => {
     if (letterNumber < wordSize) {
       const newArray = Array(6);
+
+      // copy values of the array into new array (deep copy) and include new guess
       for (let i = 0; i < 6; i = i + 1) {
         newArray[i] = [...guesses[i]]
         if (i === wordNumber) {
@@ -41,6 +47,7 @@ const Game = () => {
         }
       }
 
+      // set guesses to new letter array
       setGuesses(newArray);
       setLetterNumber(letterNumber + 1);
     } else {
@@ -48,8 +55,10 @@ const Game = () => {
     }
   }
 
+  // delete last letter guessed
   const del = () => {
     if (letterNumber > 0) {
+      // make a copy of the array and remove last letter guessed
       const newArray = Array(6);
       for (let i = 0; i < 6; i = i + 1) {
         newArray[i] = [...guesses[i]]
@@ -57,13 +66,18 @@ const Game = () => {
           newArray[i][letterNumber - 1] = '';
         }
       }
+      
+      //set guesses to the new array
       setGuesses(newArray);
       setLetterNumber(letterNumber - 1);
     } else {
+      // can't delete if we already have no letters in current word
+      // TODO: add shake?
       console.log('end of word!')
     }
   }
 
+  // returns true if the keyword has been guessed
   const wordGuessed = () => {
     if (guesses[wordNumber].join('') === keyword) {
       return true
@@ -71,14 +85,21 @@ const Game = () => {
     return false
   }
 
+  // runs when player presses the enter button to submit a guess
   const enter = () => {
+    // if correct numbers of letters guessed
     if (letterNumber === wordSize) {
+      // if guess is a valid word from word bank
       if (isValidGuess(guesses[wordNumber].join(''))) {
+        // change the status of the word to 1 for guessed 
         const tempStatuses = JSON.parse(JSON.stringify(wordStatuses));
         tempStatuses[wordNumber] = 1;
         setWordStatuses(tempStatuses);
-        console.log(wordStatuses)
+
+        // change letter statuses - unused, guessed, correct letter, incorrect place
         setLetterStatus(updateLetters(letterStatus, guesses[wordNumber], keyword));
+        
+        // check win condition 
         if (wordGuessed()) {
           alert('You won!');
         } else {
